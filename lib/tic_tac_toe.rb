840b37c5b50d3d93fe1b839460d72fe8012eab17
Dan Foley <dan@dan-foley.com>
2020-01-30 22:50:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,7 +67,7 @@ class TicTacToe
   def won?
     WIN_COMBINATIONS.each do |combo|
       if (@board[combo[0]] == "X" && @board[combo[1]] =="X" && @board[combo[2]] == "X") || (@board[combo[0]] == "O" && @board[combo[1]] =="O" && @board[combo[2]] == "O")
-        return combo
+        combo
       end
     end
     false
@@ -78,7 +78,10 @@ class TicTacToe
   end
   
   def draw?
-    false if won? || (!won? || !full?)
+    if full?
+      true
+    elsif 
+    end
   end
 
 end
