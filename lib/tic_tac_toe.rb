c0142fef7f2d4c6ba7ef583fb4ed78ee4ca4f476
Dan Foley <dan@dan-foley.com>
2020-01-30 22:45:45 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,5 +26,55 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+  
+  def input_to_index(input)
+    index = input.to_i - 1
+    # binding.pry
+  end
+  
+  def move(input_to_index, token="X")
+    @board[input_to_index] = token
+  end
+  
+  def position_taken?(input_to_index)
+    @board[input_to_index] == " " ? false : true
+  end
+  
+  def valid_move?(position)
+    true if position.between?(0,9) && @board[position] == " "
+  end
+  
+  def turn_count
+    @board.count{|i| i == "X" || i == "O"}
+  end
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def turn
+    puts "Enter a number between 1 - 9"
+    input = gets.chomp
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+  
+  def won?
+    WIN_COMBINATIONS.each do |combo|
+      if (@board[combo[0]] == "X" && @board[combo[1]] =="X" && @board[combo[2]] == "X") || (@board[combo[0]] == "O" && @board[combo[1]] =="O" && @board[combo[2]] == "O")
+        return combo
+      end
+    end
+    false
+  end
+  
+  def full?
+    true if @board.each{|i| i == "X" || i == "O"}
+  end
 
 end
